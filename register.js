065c57df784ee3aa7d421ff3018f0a115ac5ea7c
ChristineLazaro <adiraaekishea@gmail.com>
2024-05-18 00:31:36 +0800

    FIRST CODE
    MAY 17,2024

---
+++ register.js
@@ -0,0 +1,21 @@
+const formPopup = document.querySelector(".form-popup");
+const hidePopupBtn = formPopup.querySelector(".close-btn");
+const signupLoginLink = formPopup.querySelectorAll(".bottom-link a");
+
+// Show login popup
+document.addEventListener("DOMContentLoaded", () => {
+    document.body.classList.toggle("show-popup");
+});
+
+// Hide login popup
+hidePopupBtn.addEventListener("click", () => {
+    document.body.classList.remove("show-popup");
+});
+
+// Show or hide signup form
+signupLoginLink.forEach(link => {
+    link.addEventListener("click", (e) => {
+        e.preventDefault();
+        formPopup.classList[link.id === 'signup-link' ? 'add' : 'remove']("show-signup");
+    });
+});
